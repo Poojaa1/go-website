9b9ab007ff6863d3d8d606eef277a565c256c2d7
Poojaa1 <70262949+Poojaa1@users.noreply.github.com>
2021-05-19 16:48:06 +0530

    Update database.go

--- Website_1/database/database.go
+++ Website_1/database/database.go
@@ -7,10 +7,10 @@ import (
 
 //learning ===== to get the value os.Getenv("host"), run 'export host=localhost'
 const (
-	host     = "hpe.c5p6cfuydxbj.us-east-1.rds.amazonaws.com"
+	host     = "hpe-2.c5p6cfuydxbj.us-east-1.rds.amazonaws.com"
 	port     = 5432
 	user     = "postgres"
-	password = "5Peht4VxUI2V1VkBUo01"
+	password = "xz61KLLlvCUJ5Ax2xLag"
 	dbname   = "postgres"
 )
 
