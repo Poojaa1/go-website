bb9fdd2aab50cf6ee9c29ad173cb589ded7b7bb5
atul.verma <atul.verma@airtel.com>
2021-05-29 17:44:29 +0530

    Added postgres in docker compose

--- Website_1/database/database.go
+++ Website_1/database/database.go
@@ -7,11 +7,11 @@ import (
 
 //learning ===== to get the value os.Getenv("host"), run 'export host=localhost'
 const (
-	host     = "appp.cltnxlwt5pk5.us-east-1.rds.amazonaws.com"
-	port     = 5432
-	user     = "postgres"
-	password = "U7eIlSB8FsDzgG0oHYT2"
-	dbname   = "postgres"
+	host     = "localhost"
+	port     = 5430
+	user     = "postgres-dev"
+	password = "postgres-dev"
+	dbname   = "dev"
 )
 
 func StartDatabase() *sql.DB {
