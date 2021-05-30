6ddf5304d12f904a59ac562d60a5cc64995f66dd
Poojaa1 <70262949+Poojaa1@users.noreply.github.com>
2021-05-30 20:29:48 +0530

    Update database.go

--- Website_1/database/database.go
+++ Website_1/database/database.go
@@ -7,11 +7,11 @@ import (
 
 //learning ===== to get the value os.Getenv("host"), run 'export host=localhost'
 const (
-	host     = "localhost"
-	port     = 5430
-	user     = "postgres-dev"
-	password = "postgres-dev"
-	dbname   = "dev"
+	host     = "appp.cltnxlwt5pk5.us-east-1.rds.amazonaws.com"
+	port     = 5432
+	user     = "postgres"
+	password = "U7eIlSB8FsDzgG0oHYT2"
+	dbname   = "postgres"
 )
 
 func StartDatabase() *sql.DB {
